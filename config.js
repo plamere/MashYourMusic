2db3be83a29978d4a830fba331e554fbb32f81eb
Paul Lamere <paull@spotify.com>
2020-04-05 07:50:48 -0400

    Updated for q

--- config.js
+++ config.js
@@ -2,5 +2,6 @@
 
 var SPOTIFY_CLIENT_ID = '61b31b91a35f4b80939cda2f47f1025f';
 var REMOTE_SPOTIFY_REDIRECT_URI = 'http://static.echonest.com/MashYourMusic/'
+var REMOTE_SPOTIFY_REDIRECT_URI = 'http://mashyourmusic.playlistmachinery.com/'
 var LOCAL_SPOTIFY_REDIRECT_URI = 'http://localhost:8000/';
 var SPOTIFY_REDIRECT_URI = REMOTE_SPOTIFY_REDIRECT_URI;
